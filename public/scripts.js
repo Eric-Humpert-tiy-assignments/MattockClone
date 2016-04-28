2309c18641c61ac90a4edb3b7ab4831b497af88e
Eric Humpert <EricMac@Ricks-MBP.attlocal.net>
2016-04-28 11:34:25 -0500

    set up the navigation links

---
+++ public/scripts.js
@@ -0,0 +1,15 @@
+'use strict';
+if (this.MattockClone === undefined) this.MattockClone = {};
+
+(function(context) {
+
+  function start() {
+
+    //Call your code here
+    console.log('starting!', context);
+
+  }
+
+  window.MattockClone.start = start;
+
+})(window.MattockClone);
